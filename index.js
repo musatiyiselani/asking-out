d5a959714da4f98ce148c4565de654b1fc886216
musatiyiselani <musatiyiselani89@gmail.com>
2026-07-19 11:56:10 +0200

    Fixed the Yes and No button.

--- index.js
+++ index.js
@@ -18,43 +18,43 @@ function moveAndGrow() {
 
     if(count === 1) {
         nbtn.style.position = "absolute";
-        nbtn.style.right = "15.75vw";
+        nbtn.style.right = "30.5%";
         ybtn.style.transform += `scale(${1.1})`;
         nbtn.innerHTML = "Really?"
     }
     else if(count === 2) {
         nbtn.style.position = "absolute";
-        nbtn.style.right = "10vw";
+        nbtn.style.right = "21%";
         ybtn.style.transform += `scale(${1.2})`;
         nbtn.innerHTML = "Are you sure?"
     }
     else if(count === 3) {
         nbtn.style.position = "absolute";
-        nbtn.style.right = "4vw";
+        nbtn.style.right = "9%";
         ybtn.style.transform += `scale(${1.3})`;
         nbtn.innerHTML = "Please, it will be fun!"
     }
     else if(count === 4) {
         nbtn.style.position = "absolute";
-        nbtn.style.right = "0vw";
+        nbtn.style.right = "0%";
         ybtn.style.transform += `scale(${1.4})`;
         nbtn.innerHTML = "Are you serious? Really!"
     }
     else if(count === 5) {
         nbtn.style.position = "absolute";
-        nbtn.style.right = "0vw";
+        nbtn.style.right = "0%";
         ybtn.style.transform += `scale(${1.5})`;
         nbtn.innerHTML = "I promise you will love it."
     }
     else if(count === 6) {
         nbtn.style.position = "absolute";
-        nbtn.style.right = "0vw";
+        nbtn.style.right = "0%";
         ybtn.style.transform += `scale(${1.6})`;
         nbtn.innerHTML = "Come on! you will not regret."
     }
     else if(count === 7) {
         nbtn.style.position = "absolute";
-        nbtn.style.right = "0vw";
+        nbtn.style.right = "0%";
         ybtn.style.transform += `scale(${1.7})`;
         nbtn.innerHTML = "PLEASE, PLEASE, PLEASE!"
     }
