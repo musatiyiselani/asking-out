3406cdb44521c9b533f7441a617da5fbf469f332
musatiyiselani <musatiyiselani89@gmail.com>
2026-07-25 00:33:43 +0200

    Modified my buttons

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ let ybtn = document.getElementById("ybtn");
 let nbtn = document.getElementById("nbtn");
 
 let text = document.getElementById("text");
-let count = 0;
 
 const song = document.getElementById("song");
 
@@ -16,69 +15,110 @@ function startMusic() {
     if (!song.paused) return; // Already playing
 
     song.currentTime = 4;
-    song.play().catch(err => console.log(err));
+    //song.play().catch(err => console.log(err));
+    song.volume = 0.75;
 }
 
 ybtn.addEventListener("click", startMusic);
 nbtn.addEventListener("click", startMusic);
 
 
+page1.style.display = "block";
+page2.style.display = "none";
+
+
+let count = 0;
+let scale = 1.15;
+
 function moveAndGrow() {
 
-    count++;
+    let messages = [
+        "Really?",
+        "Say Yes :`)",
+        "Are you sure?",
+        "Babe, it will be fun🥹!",
+        "Baby pleeeeaaaase😭!",
+        "I promise you'll love it🥺.",
+        "You will not regret, Babe🥺",
+        "Please, Please, Please🥹!",
+    ]
+
+    let npos = [33.5, 27, 21, 7, 2, 0, 0, 0, 0]
+
+    let ypos = [43, 44, 44, 45, 45, 46, 47, 48]
+
+    for (let i = count; i <= count; i++) {
+        nbtn.style.position = "absolute";
+        nbtn.style.right = `${npos[i]}%`;
+        nbtn.style.top = `${ypos[i] - 14}%`;
+        ybtn.style.left = `${ypos[i]}%`;
+        ybtn.style.top = `${ypos[i] - 16}%`;
+        ybtn.style.transform += `scale(${scale})`;
+        nbtn.innerHTML = messages[i];
+    }
+
+    count ++;
+    scale += 0.05;
+
+    /*
 
     if(count === 1) {
         nbtn.style.position = "absolute";
-        nbtn.style.right = "31.5%";
-        ybtn.style.transform += `scale(${1.1})`;
+        nbtn.style.right = "33.5%";
+        ybtn.style.left = "43%";
+        ybtn.style.transform += `scale(${scale})`;
         nbtn.innerHTML = "Really?"
     }
     else if(count === 2) {
         nbtn.style.position = "absolute";
-        nbtn.style.right = "21%";
-        ybtn.style.transform += `scale(${1.2})`;
+        nbtn.style.right = "22%";
+        ybtn.style.left = "44%";
+        ybtn.style.transform += `scale(${scale})`;
         nbtn.innerHTML = "Are you sure?"
     }
     else if(count === 3) {
         nbtn.style.position = "absolute";
-        nbtn.style.right = "0%";
-        ybtn.style.transform += `scale(${1.3})`;
-        nbtn.innerHTML = "Please, babe it will be fun🥹!"
+        nbtn.style.right = "8.25%";
+        ybtn.style.left = "45%";
+        ybtn.style.transform += `scale(${scale})`;
+        nbtn.innerHTML = "Babe, it will be fun🥹!"
     }
     else if(count === 4) {
         nbtn.style.position = "absolute";
-        nbtn.style.right = "0%";
-        ybtn.style.transform += `scale(${1.4})`;
-        nbtn.innerHTML = "Baby pleeeeaaaaaaaaaase😭!"
+        nbtn.style.right = "3%";
+        ybtn.style.left = "46%";
+        ybtn.style.transform += `scale(${scale})`;
+        nbtn.innerHTML = "Baby pleeeeaaaase😭!"
     }
     else if(count === 5) {
         nbtn.style.position = "absolute";
         nbtn.style.right = "0%";
-        ybtn.style.transform += `scale(${1.5})`;
-        nbtn.innerHTML = "I promise you will love it🥺."
+        ybtn.style.left = "47%";
+        ybtn.style.transform += `scale(${scale})`;
+        nbtn.innerHTML = "I promise you'll love it🥺."
     }
     else if(count === 6) {
         nbtn.style.position = "absolute";
         nbtn.style.right = "0%";
-        ybtn.style.transform += `scale(${1.6})`;
+        ybtn.style.transform += `scale(${scale})`;
         nbtn.innerHTML = "You will not regret, Babe🥺"
     }
     else if(count === 7) {
         nbtn.style.position = "absolute";
         nbtn.style.right = "0%";
-        ybtn.style.transform += `scale(${1.7})`;
+        ybtn.style.transform += `scale(${scale})`;
         nbtn.innerHTML = "PLEASE, PLEASE, PLEASE🥹!"
     }
     else if(count === 8) {
         //nbtn.style.transform += `translateX(${120}px)`;
-        ybtn.style.transform += `scale(${1.25})`;
+        ybtn.style.transform += `scale(${scale})`;
     }
+
+    */
     
 }
 
 
-page1.style.display = "block";
-page2.style.display = "none";
 
 
 function swap() {
